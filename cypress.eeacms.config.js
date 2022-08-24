7429174125081fd0e4b355f53705d5e2ec4ad84c
nileshgulia1 <nileshgulia@gmail.com>
2022-08-24 16:02:57 +0530

    updated cypress configs

---
+++ cypress.eeacms.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  viewportWidth: 1280,
+  defaultCommandTimeout: 8888,
+  chromeWebSecurity: false,
+  videoUploadOnPasses: false,
+  screenshotOnRunFailure: false,
+  reporter: 'junit',
+  video: true,
+  specPattern: './node_modules/@eeacms/**/cypress/e2e/**/*.js',
+  fixturesFolder: './node_modules/@eeacms/volto-slate-zotero/cypress/fixtures',
+  retries: {
+    runMode: 8,
+    openMode: 0,
+  },
+  reporterOptions: {
+    mochaFile: 'cypress/reports/cypress-[hash].xml',
+    jenkinsMode: true,
+    toConsole: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // e2e testing node events setup code
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+});
